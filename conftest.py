aafca5a88e6d6ae443cb760b29d6b1e3fd002489
Dasha Timofeyeva <timofeyevadasha@gmail.com>
2019-02-18 16:48:26 +0300

    separated login password in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from fixture.application import Application
 
+LOGIN = "dariatimofeevatest@gmail.com"
+PASSWORD = "Bl@Sl56Er23!!7"
+
 
 @pytest.fixture()
 def app(request):
     fixture = Application()
-    fixture.session.login(login="dariatimofeevatest@gmail.com", password="Bl@Sl56Er23!!7")
+    fixture.session.login(login=LOGIN, password=PASSWORD)
     request.addfinalizer(fixture.destroy)
     return fixture
